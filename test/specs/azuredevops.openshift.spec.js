c581d2aff10703d214e4464b8eede2d2017273c6
Vishal <vishalm@users.noreply.github.com>
2021-09-20 19:52:45 +0400

    Update azuredevops.openshift.spec.js

--- test/specs/azuredevops.openshift.spec.js
+++ test/specs/azuredevops.openshift.spec.js
@@ -3,10 +3,6 @@ import azOSEDevPage from '../pages/azose.dev.page';
 chai.use(require('chai-sorted'));
 
 describe('Azure DevOps Test', () => {
-
-
-
-  // should show availability of rooms after selecting a hotel from search results filter by stars
   it('should see the openshift logo', () => {
     chai.expect(azOSEDevPage.open().isLogoPresent()).to.be.true;
   });
